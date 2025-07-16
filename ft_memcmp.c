1a8a69f4208625ce71db8c4370c7dc2522b4ea0a
nicolasgsantana <nicolas07san@gmail.com>
2025-07-16 13:23:10 -0300

    add ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 13:02:51 by nde-sant          #+#    #+#             */
+/*   Updated: 2025/07/16 13:21:50 by nde-sant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*ptr1;
+	const unsigned char	*ptr2;
+	size_t				i;
+
+	ptr1 = s1;
+	ptr2 = s2;
+	i = 0;
+	while (*ptr1 == *ptr2 && i < n)
+	{
+		if (*ptr1 != *ptr2)
+			return (*ptr1 - *ptr2);
+		ptr1++;
+		ptr2++;
+		i++;
+	}
+	return (0);
+}
