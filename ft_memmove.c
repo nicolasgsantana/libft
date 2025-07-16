cd3e16eaaea79cf8f1b23ae0b448512a7f4ac2a3
nicolasgsantana <nicolas07san@gmail.com>
2025-07-16 12:03:03 -0300

    update to use ft_mempy to copy temp array

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 10:58:21 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/07/16 11:29:24 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/07/16 11:59:23 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char		temp[n];
-	const unsigned char	*s;
-	size_t	i;
-
-	s = src;
-	i = 0;
-	while (i < n)
-	{
-		temp[i] = s[i];
-		i++;
-	}
+	ft_memcpy(&temp, src, n);
 	ft_memcpy(dest, temp, n);
 	return (dest);
 }
