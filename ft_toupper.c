0114e308ab0744362d0bf124885b00af8f2d93b8
nicolasgsantana <nicolas07san@gmail.com>
2025-07-16 10:21:49 -0300

    update logic and fix convertion

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,11 +6,9 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 18:52:23 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/07/15 19:04:38 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/07/16 10:19:27 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_isalpha(int c);
 
 static int	ft_islower(int c)
 {
@@ -19,9 +17,7 @@ static int	ft_islower(int c)
 
 int	ft_toupper(int c)
 {
-	if (ft_isalpha(c) == 0)
-		return (c);
-	else if (ft_islower(c) == 0)
-		return (c);
-	return (c + 32);
+	if (ft_islower(c) == 1)
+		return (c - 32);
+	return (c);
 }
