43ec2d15a20710b58e54a8bee0aebc171774208f
nicolasgsantana <nicolas07san@gmail.com>
2025-07-21 17:23:40 -0300

    change to find to unsigned char and handle null character

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,20 +6,27 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 18:40:00 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/07/17 19:02:15 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/07/21 17:21:07 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
-	int	last_found_index;
+	int				i;
+	int				last_found_index;
+	unsigned char	to_find;
 
 	last_found_index = -1;
 	i = 0;
+	to_find = c;
 	while (s[i] != '\0')
-		if (s[i] == c)
+	{
+		if (s[i] == to_find)
 			last_found_index = i;
+		i++;
+	}
+	if (to_find == 0)
+		last_found_index = i;
 	if (last_found_index != -1)
 		return ((char *)&s[last_found_index]);
 	return (0);
