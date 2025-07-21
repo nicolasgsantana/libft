7e3dfcb5d15362e504e58d6a2fcc11b5103b3043
nicolasgsantana <nicolas07san@gmail.com>
2025-07-21 17:02:54 -0300

    update to find to unsigned char

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,18 +6,20 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 17:49:08 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/07/17 19:02:04 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/07/21 17:01:43 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strchr(const char *s, int c)
 {
 	int	i;
+	unsigned char	to_find;
 
+	to_find = c;
 	i = 0;
-	while (s[i] != c && s[i] != '\0')
+	while (s[i] != to_find && s[i] != '\0')
 		i++;
-	if (s[i] == c)
+	if (s[i] == to_find)
 		return ((char *)&s[i]);
 	else
 		return (0);
