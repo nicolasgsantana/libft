c2f445a19f59fe23253ed5775546a375c1b97d34
nicolasgsantana <nicolas07san@gmail.com>
2025-07-21 14:12:05 -0300

    update memmove implementation

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,20 +6,37 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 10:58:21 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/07/18 12:34:08 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/07/21 14:09:40 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
-
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+#include <string.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*temp;
+	unsigned char		*d;
+	const unsigned char	*s;
+	size_t				i;
 
-	ft_bzero(temp, n);
-	ft_memcpy(&temp, src, N);
-	ft_memcpy(dest, temp, N);
-	return (dest);
+	d = dest;
+	s = src;
+	i = 0;
+	if (d == s)
+		return (dest);
+	else if (d < s)
+		while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	else
+	{
+		while (i <= n)
+		{
+			d[n-i] = s[n-i];
+			i++;
+		}
+	}
+	return ((void *)d);
 }
