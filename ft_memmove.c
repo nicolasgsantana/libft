9b6abc9adf541fcf275f4992884061a7739e513e
nicolasgsantana <nicolas07san@gmail.com>
2025-07-21 16:32:42 -0300

    fix bug at the backwards loop

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 10:58:21 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/07/21 14:09:40 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/07/21 16:31:17 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,15 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		return (dest);
 	else if (d < s)
 		while (i < n)
-	{
-		d[i] = s[i];
-		i++;
-	}
-	else
-	{
-		while (i <= n)
 		{
-			d[n-i] = s[n-i];
+			d[i] = s[i];
 			i++;
 		}
-	}
+	else
+		while (i < n)
+		{
+			i++;
+			d[n-i] = s[n-i];
+		}
 	return ((void *)d);
 }
