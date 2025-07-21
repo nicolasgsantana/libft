8b92bcd5804c6ea6404e25b9aa5d9e0b9593f385
nicolasgsantana <nicolas07san@gmail.com>
2025-07-21 16:37:45 -0300

    fix while loop bug to check first iteration

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 12:07:22 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/07/21 16:05:57 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/07/21 16:36:44 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,9 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (src[i++] == to_find)
+		if (src[i] == to_find)
 			return ((unsigned char *)&src[i]);
+		i++;
 	}
 	return (NULL);
 }
