691d18a9f3901f737a2c1ec2a0ceb8c1e48f553f
nicolasgsantana <nicolas07san@gmail.com>
2025-07-22 11:51:03 -0300

    fix len check and hangle empty little case

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 10:55:46 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/07/18 12:18:41 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/07/22 11:49:43 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,21 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	j;
 
 	i = 0;
-	j = 0;
-	while (big[i] != '\0')
+	if (little [i] == '\0')
+		return ((char *)big);
+	while (big[i] != '\0' && i < len)
 	{
+		j = 0;
 		if (big[i] == little[j])
 		{
-			while (j < len && little[j] != '\0')
+			while (little[j] != '\0' && i + j < len)
 			{
-				j++;
 				if (big[i + j] != little[j])
 					break ;
 				if (j == len || little[j + 1] == '\0')
 					return ((char *)&big[i]);
+				j++;
 			}
-			j = 0;
 		}
 		i++;
 	}
