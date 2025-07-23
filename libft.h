9807182e1b5f5f8cd4b9ade11d401d3cfe0637a7
nicolasgsantana <nicolas07san@gmail.com>
2025-07-23 11:57:34 -0300

    add ft_itoa

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 15:10:14 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/07/22 19:08:22 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/07/23 11:12:31 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+char	*ft_itoa(int n);
 
 #endif
