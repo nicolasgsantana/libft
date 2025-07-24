b1146d507833b0336734517adabf903887ee666b
nicolasgsantana <nicolas07san@gmail.com>
2025-07-24 13:36:35 -0300

    fix possible memory leak

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,14 +6,20 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 12:04:23 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/07/23 12:22:51 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/07/24 13:36:07 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
 
 void	ft_putstr_fd(char *s, int fd);
 char	*ft_itoa(int n);
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	char	*converted_n;
+
+	converted_n = ft_itoa(n);
+	ft_putstr_fd(converted_n, fd);
+	free(converted_n);
 }
